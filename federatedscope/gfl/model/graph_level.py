e179fd5caf0e45a391b7ebd526858307b4a6a7af
Michael <michael.schulten@yahoo.de>
2022-10-18 12:19:10 +0200

    ...

--- federatedscope/gfl/model/graph_level.py
+++ federatedscope/gfl/model/graph_level.py
@@ -1,16 +1,10 @@
-from typing import List
-
 import torch
 import torch.nn.functional as F
-from torch import Tensor
 from torch.nn import Linear, Sequential, BatchNorm1d
 from torch_geometric.data import Data
 from torch_geometric.data.batch import Batch
-from torch_geometric.utils import degree
-from torch_geometric.utils import negative_sampling
 from torch_geometric.nn.glob import global_add_pool, global_mean_pool, \
     global_max_pool
-from wandb.util import np
 
 from federatedscope.gfl.model.gcn import GCN_Net
 from federatedscope.gfl.model.gine import GINE_Net
@@ -18,8 +12,7 @@ from federatedscope.gfl.model.sage import SAGE_Net
 from federatedscope.gfl.model.gat import GAT_Net
 from federatedscope.gfl.model.gin import GIN_Net
 from federatedscope.gfl.model.gpr import GPR_Net
-# graph_level_domain_sep_gine
-EPS = 1e-15
+
 EMD_DIM = 200
 
 
@@ -65,7 +58,6 @@ class GNN_Net_Graph(torch.nn.Module):
         # Embedding (pre) layer
         self.encoder_atom = AtomEncoder(in_channels, hidden)
         self.encoder = Linear(in_channels, hidden)
-        self.decoder = InnerProductDecoder()
         # GNN layer
         if gnn == 'gcn':
             self.gnn = GCN_Net(in_channels=hidden,
@@ -86,16 +78,11 @@ class GNN_Net_Graph(torch.nn.Module):
                                max_depth=max_depth,
                                dropout=dropout)
         elif gnn == 'gin':
-            self.local_gnn = GINE_Net(in_channels=hidden,
+            self.gnn = GINE_Net(in_channels=hidden,
                                out_channels=hidden,
                                hidden=hidden,
                                max_depth=max_depth,
                                dropout=dropout)
-            self.global_gnn = GINE_Net(in_channels=hidden,
-                                out_channels=hidden,
-                                hidden=hidden,
-                                max_depth=max_depth,
-                                dropout=dropout)
         elif gnn == 'gpr':
             self.gnn = GPR_Net(in_channels=hidden,
                                out_channels=hidden,
@@ -135,28 +122,8 @@ class GNN_Net_Graph(torch.nn.Module):
             x = self.encoder(x)
 
         edge_attr = self.emb(edge_attr).mean(1)
-        x_local = self.local_gnn(x, edge_index, edge_attr)
-        x_global = self.global_gnn(x, edge_index, edge_attr)
-
-        sizes = degree(batch, dtype=torch.long).tolist()
-
-        unbatched_local = x_local.split(sizes, 0)
-        unbatched_global = x_global.split(sizes, 0)
-        unbatched_edge_index = unbatch_edge_index(edge_index, batch)
-
-        rec_loss = None
-        for i in range(len(unbatched_local)):
-            # A_pred = dot_product_decode(unbatched_enc_pr[i] + unbatched_enc[i])
-            if rec_loss is None:
-                rec_loss = self.recon_loss(unbatched_local[i] + unbatched_global[i], unbatched_local[i].size(dim=0),
-                                           unbatched_edge_index[i], neg_edge_index=None, )
-            else:
-                rec_loss += self.recon_loss(unbatched_local[i] + unbatched_global[i], unbatched_local[i].size(dim=0),
-                                            unbatched_edge_index[i], neg_edge_index=None, )
-
-        rec_loss = rec_loss / len(unbatched_local)
-
-        x = self.pooling(x_global, batch)
+        x = self.gnn(x, edge_index, edge_attr)
+        x = self.pooling(x, batch)
         x = self.linear(x)
         if x.size(0) > 1:
             x = self.bn_linear(x).relu()
@@ -164,116 +131,4 @@ class GNN_Net_Graph(torch.nn.Module):
             x = x.relu()
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.clf(x)
-        return x, x_local, x_global, rec_loss
-
-    def recon_loss(self, z, num_nodes, pos_edge_index, neg_edge_index=None):
-        r"""Given latent variables :obj:`z`, computes the binary cross
-        entropy loss for positive edges :obj:`pos_edge_index` and negative
-        sampled edges.
-
-        Args:
-            z (Tensor): The latent space :math:`\mathbf{Z}`.
-            pos_edge_index (LongTensor): The positive edges to train against.
-            neg_edge_index (LongTensor, optional): The negative edges to train
-                against. If not given, uses negative sampling to calculate
-                negative edges. (default: :obj:`None`)
-        """
-
-        pos_loss = -torch.log(
-            self.decoder(z, pos_edge_index, sigmoid=True) + EPS).mean()
-
-        if neg_edge_index is None:
-            try:
-                neg_edge_index = negative_sampling(pos_edge_index, num_nodes=num_nodes)
-            except:
-                pass
-        if neg_edge_index is not None:
-
-            neg_loss = -torch.log(1 -
-                                  self.decoder(z, neg_edge_index, sigmoid=True) +
-                                  EPS).mean()
-        else:
-            neg_loss = 0
-
-        return pos_loss + neg_loss
-
-
-
-def dot_product_decode(Z):
-    A_pred = torch.sigmoid(torch.matmul(Z, Z.t()))
-    return A_pred
-
-
-def glorot_init(input_dim, output_dim):
-    init_range = np.sqrt(6.0 / (input_dim + output_dim))
-    initial = torch.rand(input_dim, output_dim) * 2 * init_range - init_range
-    return torch.nn.Parameter(initial)
-
-
-class InnerProductDecoder(torch.nn.Module):
-    """The inner product decoder from the `"Variational Graph Auto-Encoders"
-    <https://arxiv.org/abs/1611.07308>`_ paper
-
-    .. math::
-        \sigma(\mathbf{Z}\mathbf{Z}^{\top})
-
-    where :math:`\mathbf{Z} \in \mathbb{R}^{N \times d}` denotes the latent
-    space produced by the encoder."""
-    def __init__(self):
-        super().__init__()
-    def forward(self, z, edge_index, sigmoid=True, neg_print =False):
-        r"""Decodes the latent variables :obj:`z` into edge probabilities for
-        the given node-pairs :obj:`edge_index`.
-
-        Args:
-            z (Tensor): The latent space :math:`\mathbf{Z}`.
-            sigmoid (bool, optional): If set to :obj:`False`, does not apply
-                the logistic sigmoid function to the output.
-                (default: :obj:`True`)
-        """
-        value = (z[edge_index[0]] * z[edge_index[1]]).sum(dim=1)
-
-        return torch.sigmoid(value) if sigmoid else value
-
-    def forward_all(self, z, sigmoid=True):
-        r"""Decodes the latent variables :obj:`z` into a probabilistic dense
-        adjacency matrix.
-
-        Args:
-            z (Tensor): The latent space :math:`\mathbf{Z}`.
-            sigmoid (bool, optional): If set to :obj:`False`, does not apply
-                the logistic sigmoid function to the output.
-                (default: :obj:`True`)
-        """
-        adj = torch.matmul(z, z.t())
-        return torch.sigmoid(adj) if sigmoid else adj
-
-def unbatch_edge_index(edge_index: Tensor, batch: Tensor) -> List[Tensor]:
-    r"""Splits the :obj:`edge_index` according to a :obj:`batch` vector.
-
-    Args:
-        edge_index (Tensor): The edge_index tensor. Must be ordered.
-        batch (LongTensor): The batch vector
-            :math:`\mathbf{b} \in {\{ 0, \ldots, B-1\}}^N`, which assigns each
-            node to a specific example. Must be ordered.
-
-    :rtype: :class:`List[Tensor]`
-
-    Example:
-
-        >>> edge_index = torch.tensor([[0, 1, 1, 2, 2, 3, 4, 5, 5, 6],
-        ...                            [1, 0, 2, 1, 3, 2, 5, 4, 6, 5]])
-        >>> batch = torch.tensor([0, 0, 0, 0, 1, 1, 1])
-        >>> unbatch_edge_index(edge_index, batch)
-        (tensor([[0, 1, 1, 2, 2, 3],
-                [1, 0, 2, 1, 3, 2]]),
-        tensor([[0, 1, 1, 2],
-                [1, 0, 2, 1]]))
-    """
-    deg = degree(batch, dtype=torch.int64)
-    ptr = torch.cat([deg.new_zeros(1), deg.cumsum(dim=0)[:-1]], dim=0)
-
-    edge_batch = batch[edge_index[0]]
-    edge_index = edge_index - ptr[edge_batch]
-    sizes = degree(edge_batch, dtype=torch.int64).cpu().tolist()
-    return edge_index.split(sizes, dim=1)
+        return x
