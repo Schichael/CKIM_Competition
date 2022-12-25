98f8e696af5660756143a14e6ab176a18708cb89
michael <michael.schulten@yahoo.de>
2022-12-25 16:04:53 +0100

    use new lrs

--- main_isolated.py
+++ main_isolated.py
@@ -57,7 +57,7 @@ def train(client, lr):
 
 if __name__ == '__main__':
     clients = range(1, 16 + 1)
-    lrs = [0.1, 0.01, 0.001]
+    lrs = [0.05, 0.01, 0.005, 0.001]
     num_trainings = 3
     for client in clients:
         for lr in lrs:
