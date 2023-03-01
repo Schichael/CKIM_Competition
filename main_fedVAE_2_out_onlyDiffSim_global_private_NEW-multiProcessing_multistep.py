8aab2a0a43e207198a6e9fb0839a10cfbb8d4381
michael <michael.schulten@yahoo.de>
2023-03-01 18:53:57 +0100

    use frobenius loss model

--- main_fedVAE_2_out_onlyDiffSim_global_private_NEW-multiProcessing_multistep.py
+++ main_fedVAE_2_out_onlyDiffSim_global_private_NEW-multiProcessing_multistep.py
@@ -138,18 +138,18 @@ def tmp(a):
 
 if __name__ == '__main__':
 
-    num_trainings = 2
+    num_trainings = 3
     kld_ne_imps = [0] #A
     diff_imps = [0.001]   #NOW 0.0001, 0
     diff_interm_imp = 0.001 #F    HERE  [0.0001, 0.001]
     diff_local_imp = 0.001 #G
     csd_imp = 10 #H
-    sims = [0.1] #I    HERE   [0, 1]   #NOW 0., 1
+    sims = [1] #I    HERE   [0, 1]   #NOW 0., 1
     sim_losses = ["mse"]
 
     # lrs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
     lrs = [0.1]
-    pool = multiprocessing.Pool(2)
+    pool = multiprocessing.Pool(3)
     processes = []
     for lr in lrs:
         for sim in sims:
@@ -157,7 +157,6 @@ if __name__ == '__main__':
                 for sim_loss in sim_losses:
                     for kld_ne_imp in kld_ne_imps:
                         for i in range(num_trainings):
-                            setup_seed(i+3)
                             processes.append(pool.apply_async(train, args=(lr, kld_ne_imp, diff_imp, diff_imp, sim, csd_imp, sim_loss)))
     result = [p.get() for p in processes]
 
