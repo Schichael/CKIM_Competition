45545fceacabf5e6f44152c3d8768bc98be61c5f
michael <michael.schulten@yahoo.de>
2023-03-05 19:44:31 +0100

    use cosine loss

--- main_fedVAE_2_out_onlyCosineDiffSim_global_private_NEW-multiProcessing_multistep.py
+++ main_fedVAE_2_out_onlyCosineDiffSim_global_private_NEW-multiProcessing_multistep.py
@@ -138,13 +138,13 @@ def tmp(a):
 
 if __name__ == '__main__':
 
-    num_trainings = 1
+    num_trainings = 5
     kld_ne_imps = [0] #A
-    diff_imps = [0.1]   #NOW 0.0001, 0
+    diff_imps = [0.01]   #NOW 0.0001, 0
     diff_interm_imp = 0.001 #F    HERE  [0.0001, 0.001]
     diff_local_imp = 0.001 #G
     csd_imp = 10 #H
-    sims = [5] #I    HERE   [0, 1]   #NOW 0., 1
+    sims = [1] #I    HERE   [0, 1]   #NOW 0., 1
     sim_losses = ["mse"]
 
     # lrs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
@@ -157,7 +157,7 @@ if __name__ == '__main__':
                 for sim_loss in sim_losses:
                     for kld_ne_imp in kld_ne_imps:
                         for i in range(num_trainings):
-                            setup_seed(i)
+                            setup_seed(i+3)
                             processes.append(pool.apply_async(train, args=(lr, kld_ne_imp, diff_imp, diff_imp, sim, csd_imp, sim_loss)))
     result = [p.get() for p in processes]
 
