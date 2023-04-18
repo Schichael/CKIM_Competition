2d66406a43766f0db9078309f09a9d2272975a92
michael <michael.schulten@yahoo.de>
2023-04-18 21:48:29 +0200

    use MSE sim

--- main_fedVAE_1_out_only_COSINE_Diff_MSE_SIM_NEW-multiProcessing.py
+++ main_fedVAE_1_out_only_COSINE_Diff_MSE_SIM_NEW-multiProcessing.py
@@ -189,10 +189,10 @@ def tmp(a):
 
 if __name__ == '__main__':
 
-    num_trainings = 1
+    num_trainings = 3
     kld_ne_imps = [0] #A
-    diff_imps = [0.1]  # Now 0.0001
-    sim_imps = [1, 0.1, 0.01, 0.001, 0]
+    diff_imps = [0.005, 0.05]  # Now 0.0001
+    sim_imps = [1, 0.1, 0.01]
 
     #diff_imps = [0.1]
     #diff_global_imps = [0] #F    HERE  [0.0001, 0.001]
@@ -203,7 +203,7 @@ if __name__ == '__main__':
 
     # lrs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
     lrs = [0.05]
-    pool = multiprocessing.Pool(5)
+    pool = multiprocessing.Pool(6)
     processes = []
     for lr in lrs:
         for diff_imp in diff_imps:
