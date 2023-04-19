fbe9b4abde2bc20cbbe930517585da0e2105604a
michael <michael.schulten@yahoo.de>
2023-04-19 11:29:03 +0200

    use PROX sim

--- main_fedVAE_1_out_only_COSINE_Diff_COSINE_SIM_NEW-multiProcessing.py
+++ main_fedVAE_1_out_only_COSINE_Diff_COSINE_SIM_NEW-multiProcessing.py
@@ -124,7 +124,7 @@ def train(lr, kld_ne_imp, diff_imp_global, diff_imp_local, sim_imp, csd_imp):
     # init_cfg.data.subdirectory = 'graph_dt_backup/processed'
     # init_cfg.merge_from_list(args.opts)
     init_cfg.data.save_dir = \
-        'Graph-DC_1_out_only_COSINE_Diff_COSINE_SIM_NEW_single_run_lr_' + str(lr).replace(
+        'Graph-DC_1_out_only_COSINE_Diff_COSINE_SIM_NEW_multi_run_lr_' + str(lr).replace(
             '.', '_') + '_A'+ str(kld_ne_imp).replace('.', '_') + \
     '_F' + str(diff_imp_global).replace('.', '_') + '_F' + str(diff_imp_local).replace(
         '.', '_') + '_H' + str(csd_imp).replace(
@@ -190,7 +190,7 @@ if __name__ == '__main__':
     num_trainings = 3
     kld_ne_imps = [0] #A
     diff_imps = [0.005, 0.05]  # Now 0.0001
-    sim_imps = [0.1, 0.01]
+    sim_imps = [1]
     #diff_imps = [0.1]
     #diff_global_imps = [0] #F    HERE  [0.0001, 0.001]
     #diff_local_imps = [0] #G
@@ -202,7 +202,7 @@ if __name__ == '__main__':
 
     # lrs = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5]
     lrs = [0.05]
-    pool = multiprocessing.Pool(6)
+    pool = multiprocessing.Pool(2)
     processes = []
     for lr in lrs:
         for diff_imp in diff_imps:
